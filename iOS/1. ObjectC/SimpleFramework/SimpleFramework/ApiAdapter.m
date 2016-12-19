26048bbd5a67804e642318f6c7cf08d04dcd6fcc
zapieah <zapieah@gmail.com>
2016-12-19 18:00:39 +0900

    update

---
+++ iOS/1. ObjectC/SimpleFramework/SimpleFramework/ApiAdapter.m
@@ -0,0 +1,17 @@
+//
+//  ApiAdapter.m
+//  SimpleFramework
+//
+//  Created by bogon on 2016. 11. 22..
+//  Copyright © 2016년 com.bogon. All rights reserved.
+//
+
+#import "ApiAdapter.h"
+
+@implementation ApiAdapter
+
+-(void) sayHello
+{
+    NSLog(@"Hello");
+}
+@end
---
+++ iOS/1. ObjectC/SimpleFramework/SimpleFramework/SimpleFramework.h
@@ -0,0 +1,20 @@
+//
+//  SimpleFramework.h
+//  SimpleFramework
+//
+//  Created by bogon on 2016. 11. 22..
+//  Copyright © 2016년 com.bogon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleFramework.
+FOUNDATION_EXPORT double SimpleFrameworkVersionNumber;
+
+//! Project version string for SimpleFramework.
+FOUNDATION_EXPORT const unsigned char SimpleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleFramework/PublicHeader.h>
+
+
+#import "SimpleFramework/ApiAdapter.h"
