f2e8bdc98c444c52f280ca2a484389d185eab384
zapieah <zapieah@gmail.com>
2016-12-29 17:39:27 +0900

    code clean up

--- iOS/1. ObjectC/ProgrammingInObjectiveC(Ch.1-7)/ProgrammingInObjectiveC/main.m
+++ iOS/1. ObjectC/ProgrammingInObjectiveC(Ch.1-7)/ProgrammingInObjectiveC/main.m
@@ -25,7 +25,7 @@
     int denominator;
 }
 -(void) print {
-    NSLog (@"%i/%i", numerator, denominator);
+    NSLog(@"%i/%i", numerator, denominator);
 }
 -(void) setNumerator: (int) n {
     numerator = n;
@@ -201,11 +201,11 @@ int main(int argc, const char * argv[]) {
         int n3, triangularNumber3;
         NSLog(@"TABLE OF TRIANGULAR NUMBERS");
         NSLog(@" n Sum from 1 to n");
-        NSLog(@"-- ---------------");
+        NSLog(@"------------------");
         triangularNumber3 = 0;
         for (n3 = 1; n3 <= 10; ++n3 ) {
             triangularNumber3 += n3;
-            NSLog (@" %i       %i", n3, triangularNumber3);
+            NSLog(@" %i       %i", n3, triangularNumber3);
         }
         int n4, number4, triangularNumber4;
         NSLog(@"What triangular number do you want?");
@@ -270,8 +270,8 @@ int main(int argc, const char * argv[]) {
         [bFraction print];
         NSLog(@"%g", [bFraction convertToNum]);
         int number_to_test, remainder;
-        NSLog (@"Enter your number to be tested: ");
-        scanf ("%i", &number_to_test);
+        NSLog(@"Enter your number to be tested: ");
+        scanf("%i", &number_to_test);
         remainder = number_to_test % 2;
         if (remainder == 0)
             NSLog (@"The number is even.");
@@ -282,9 +282,9 @@ int main(int argc, const char * argv[]) {
         scanf("%i", &number_to_test2);
         remainder2 = number_to_test2 % 2;
         if (remainder2 == 0)
-            NSLog (@"The number is even.");
+            NSLog(@"The number is even.");
         else
-            NSLog (@"The number is odd.");
+            NSLog(@"The number is odd.");
         int year, rem_4, rem_100, rem_400;
         NSLog(@"Enter the year to be tested: ");
         scanf("%i", &year);
@@ -292,9 +292,9 @@ int main(int argc, const char * argv[]) {
         rem_100 = year % 100;
         rem_400 = year % 400;
         if ((rem_4 == 0 && rem_100 != 0) || rem_400 == 0)
-            NSLog (@"It's a leap year.");
+            NSLog(@"It's a leap year.");
         else
-            NSLog (@"Nope, it's not a leap year.");
+            NSLog(@"Nope, it's not a leap year.");
         int number8, sign8;
         NSLog(@"Please type in a number: ");
         scanf("%i", &number8);
@@ -331,8 +331,8 @@ int main(int argc, const char * argv[]) {
         double value10, value11;
         char operator10;
         Calculator *deskCalc3 = [[Calculator alloc] init];
-        NSLog (@"Type in your expression.");
-        scanf ("%lf %c %lf", &value10, &operator10, &value11);
+        NSLog(@"Type in your expression.");
+        scanf("%lf %c %lf", &value10, &operator10, &value11);
         [deskCalc3 setAccumulator: value10];
         if (operator10 == '+')
             [deskCalc3 add: value11];
@@ -340,19 +340,20 @@ int main(int argc, const char * argv[]) {
             [deskCalc3 subtract: value11];
         else if (operator10 == '*')
             [deskCalc3 multiply: value11];
-        else if (operator10 == '/')
+        else if (operator10 == '/') {
             if (value11 == 0)
                 NSLog(@"Division by zero.");
             else
                 [deskCalc3 divide: value11];
-            else
-                NSLog (@"Unknown operator.");
+        }
+        else
+            NSLog(@"Unknown operator.");
         NSLog (@"%.2f", [deskCalc3 accumulator]);
         double val1, val2;
         char oper;
         Calculator *deskCalc4 = [[Calculator alloc] init];
-        NSLog (@"Type in your expression.");
-        scanf ("%lf %c %lf", &val1, &oper, &val2);
+        NSLog(@"Type in your expression.");
+        scanf("%lf %c %lf", &val1, &oper, &val2);
         [deskCalc4 setAccumulator: val1];
         switch (oper) {
             case '+':
@@ -369,14 +370,16 @@ int main(int argc, const char * argv[]) {
                 break;
             default:
                 NSLog (@"Unknown operator.");
-            break; }
-        NSLog (@"%.2f", [deskCalc4 accumulator]);
+                break; 
+        }
+        NSLog(@"%.2f", [deskCalc4 accumulator]);
         int p, dd, isPrime;
         for (p = 2; p <= 50; ++p) {
             isPrime = 1;
-            for (dd = 2; dd < p; ++dd)
+            for (dd = 2; dd < p; ++dd) {
                 if (p % dd == 0)
                     isPrime = 0;
+            }
             if (isPrime != 0)
                 NSLog(@"%i ", p);
         }
@@ -384,9 +387,10 @@ int main(int argc, const char * argv[]) {
         BOOL isPrime3;
         for (p3 = 2; p3 <= 50; ++p3) {
             isPrime3 = YES;
-            for (d3 = 2; d3 < p3; ++d3)
+            for (d3 = 2; d3 < p3; ++d3) {
                 if (p3 % d3 == 0)
                     isPrime3 = NO;
+            }
             if (isPrime3 == YES)
                 NSLog (@"%i ", p3);
         }
@@ -396,7 +400,7 @@ int main(int argc, const char * argv[]) {
     myFractionClass.numerator = 1;
     myFractionClass.denominator = 3;
     // display the FractionClass's numerator and denominator
-    NSLog (@"The numerator is %i, and the denominator is %i", myFractionClass.numerator, myFractionClass.denominator);
+    NSLog(@"The numerator is %i, and the denominator is %i", myFractionClass.numerator, myFractionClass.denominator);
     FractionClass *aFractionClass = [[FractionClass alloc] init];
     [aFractionClass setTo: 100 over: 200];
     [aFractionClass print];
@@ -407,9 +411,9 @@ int main(int argc, const char * argv[]) {
     [bFractionClass setTo: 1 over: 4];
     [cFractionClass setTo: 1 over: 2];
     [bFractionClass print];
-    NSLog (@"+");
+    NSLog(@"+");
     [cFractionClass print];
-    NSLog (@"=");
+    NSLog(@"=");
     [bFractionClass add: cFractionClass];
     [bFractionClass reduce];
     [bFractionClass print];
@@ -417,9 +421,9 @@ int main(int argc, const char * argv[]) {
     [bFractionClass setTo: 1 over: 4];
     [cFractionClass setTo: 1 over: 2];
     [bFractionClass print];
-    NSLog (@"+");
+    NSLog(@"+");
     [cFractionClass print];
-    NSLog (@"=");
+    NSLog(@"=");
     resultFraction = [bFractionClass add: cFractionClass];
     [resultFraction print];
     return 0;
